003385d1c6712e7f4a20df46ac711cdb0eeace46
NebiBine <bine.tavcar@gmail.com>
2025-09-23 20:30:46 +0200

    debug new profile

--- backend/app/services/servProfile.py
+++ backend/app/services/servProfile.py
@@ -1,9 +1,9 @@
 from .servProfileDb import newProfile
 from flask import jsonify
 
-def servNewProfile(name, username, birth, phone, street, city, country, zip):
+def servNewProfile(userId, name, username, birth, phone, street, city, country, zip):
     try:
-        newProfile(name, username, birth, phone, street, city, country, zip)
+        newProfile(userId, name, username, birth, phone, street, city, country, zip)
         return jsonify({"success": True, "message":"Profile added!"}), 200
     except:
         return jsonify({"success": False, "message": "Error with adding the profile!"}), 500
