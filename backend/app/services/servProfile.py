ad56d5bfb55c02b9e91ea87cb6193c18e000639d
NebiBine <bine.tavcar@gmail.com>
2025-09-29 18:12:11 +0200

    pfp saving backend

--- backend/app/services/servProfile.py
+++ backend/app/services/servProfile.py
@@ -1,10 +1,22 @@
 from .servProfileDb import newProfile
 from flask import jsonify
+import os
 
-def servNewProfile(userId, name, username, birth, phone, street, city, country, zip):
+def servNewProfile(userId, name, username, birth, phone, street, city, country, zip, pfp):
     try:
-        newProfile(userId, name, username, birth, phone, street, city, country, zip)
+        pfpDir = os.path.join("databases", "pfps")
+        os.makedirs(pfpDir, exist_ok=True)
+        extension = os.path.splitext(pfp.filename)[1]
+        pfpName = f"pfp{userId}{extension}"
+        pfpPath = os.path.join(pfpDir, pfpName)
+        pfp.save(pfpPath)
+        pfpPathg = f"../../databases/pfps{pfpName}"
+    except:
+        pass
+        
+    try:
+        newProfile(userId, name, username, birth, phone, street, city, country, zip, pfpPath)
         return jsonify({"success": True, "message":"Profile added!"}), 200
     except:
-        return jsonify({"success": False, "message": "Error with adding the profile!"}), 500
+        return jsonify({"success": False, "message": "Error with adding the profile!"}), 501
     
