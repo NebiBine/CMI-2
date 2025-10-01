dc20afd9471c5293e9b68d975a1587c2f1947f15
NebiBine <bine.tavcar@gmail.com>
2025-10-01 19:58:16 +0200

    debug

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
+
 def createApp():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "123"
+
+
 
     from app.routes.authRoutes import authBp
     app.register_blueprint(authBp)
