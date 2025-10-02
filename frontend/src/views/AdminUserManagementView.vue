2a1923ecdb9f125f99c5463f986573f08d060826
Nace <nacerozman3@gmail.com>
2025-10-02 19:15:01 +0200

    testno dodan modal na vsakem userju

--- frontend/src/views/AdminUserManagementView.vue
+++ frontend/src/views/AdminUserManagementView.vue
@@ -2,21 +2,27 @@
 import { ref, onMounted } from "vue";
 import '../assets/styles/mainStyle.css'
 import axios from 'axios'
+import { NModal, NButton } from "naive-ui";
 
 const users = ref([])
+const openUserModal = (user) => {
+  selectedUser.value = user; 
+  showModal.value = true;      
+};
+const showModal = ref(false);
+const selectedUser = ref(null);
 
 
-
-const getAllUsers = async() => {
-    try{
+const getAllUsers = async () => {
+    try {
         const response = await axios.get(
-      'http://localhost:8080/data/getAllUsers', {
-      withCredentials: true
-    })
-    console.log(response.data)
-    users.value = response.data
+            'http://localhost:8080/data/getAllUsers', {
+            withCredentials: true
+        })
+        console.log(response.data)
+        users.value = response.data
     }
-    catch(error){
+    catch (error) {
         console.log(error)
     }
 }
@@ -26,11 +32,27 @@ onMounted(() => {
 </script>
 <template>
     <h1>User Management</h1>
-    
-    <!--LISTAM USE USERJE-->
-    <ul>
-        <li v-for="user in users" :key="user.id">{{ user.username }},{{ user.email }}</li>
-    </ul>
 
+    <!--LISTAM USE USERJE-->
+    <div class="users_list">
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <b>Username:</b>
+        {{ user.username }}
+        <b> Email: </b>{{ user.email }}
+        <a href="#" @click.prevent="openUserModal(user)">
+        More Info
+        </a>
+      </li>
+    </ul>
+  </div>
+  <n-modal v-model:show="showModal" preset="dialog" title="User Details">
+    <template #default>
+      <p><b>Username:</b> {{ selectedUser?.username }}</p>
+      <p><b>Email:</b> {{ selectedUser?.email }}</p>
+      <p><b>ID:</b> {{ selectedUser?.id }}</p>
+      
+    </template>
+  </n-modal>
 
 </template>
