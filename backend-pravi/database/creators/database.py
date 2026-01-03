e9200b8a95158d6d54db85a54c664885549e60d7
NebiBine <bine.tavcar@gmail.com>
2026-01-03 14:31:52 +0100

    reintegracija backend

--- backend-pravi/database/creators/database.py
+++
@@ -1,5 +0,0 @@
-from odmantic import AIOEngine
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient("mongodb://localhost:27017")
-engine = AIOEngine(motor_client=client, database="Cmi-city")
