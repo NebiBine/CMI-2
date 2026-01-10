f50102425a837b9d5cd80bb52558963046f1c538
Nace1R <nacerozman3@gmail.com>
2026-01-10 18:43:04 +0100

    Fix user points retrieval in RewardsView

--- frontend/src/views/RewardsView.vue
+++ frontend/src/views/RewardsView.vue
@@ -1,20 +1,24 @@
 <script setup>
-    import '../assets/styles/mainStyle.css'
-    import { ref, onMounted } from 'vue';
-    import axios from 'axios';
+import '../assets/styles/mainStyle.css'
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
-    const userPoints = ref(0);
+const userPoints = ref(0);
 
-    await function getUserPoints(){
-        try{
-            const response = await axios.get("https://localhost:8000/poll-reward/getUserPoints",
-            { withCredentials: true });
-            userPoints.value = response.data.points;
-        }
-        catch(error){
-
-        }
+async function getUserPoints(){
+    try{
+        const response = await axios.get("http://localhost:8000/poll-reward/getUserPoints",
+        { withCredentials: true });
+        userPoints.value = response.data;
     }
+    catch(error){
+        console.log(error)
+    }
+}
+
+onMounted(() => {
+    getUserPoints();
+});
 </script>
 <template>
     <h1>Rewards</h1>
