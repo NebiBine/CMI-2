d1cb924bb328aca6bf6ba31e0ea309f2a86c3a2e
Nace1R <nacerozman3@gmail.com>
2026-01-10 18:37:30 +0100

    Add user points retrieval and display in RewardsView

--- frontend/src/views/RewardsView.vue
+++ frontend/src/views/RewardsView.vue
@@ -3,11 +3,23 @@
     import { ref, onMounted } from 'vue';
     import axios from 'axios';
 
+    const userPoints = ref(0);
+
+    await function getUserPoints(){
+        try{
+            const response = await axios.get("https://localhost:8000/poll-reward/getUserPoints",
+            { withCredentials: true });
+            userPoints.value = response.data.points;
+        }
+        catch(error){
+
+        }
+    }
 </script>
 <template>
     <h1>Rewards</h1>
     <p>Look through the available rewards here and redeem them!</p>
-
+    <p>Your current points: {{ userPoints }}</p>
     <div v-for="reward in rewards">
         <!--TODO: PRIKAZI REWARDE TUKAJ V TEM DIVU--> 
     </div>
